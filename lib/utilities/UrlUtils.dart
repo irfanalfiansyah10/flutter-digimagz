cd75a99104a4f1a840b353e05d57d862850acb13
Rey <firnasreyhan@gmail.com>
2021-02-26 18:54:43 +0700

    Up

--- lib/utilities/UrlUtils.dart
+++ lib/utilities/UrlUtils.dart
@@ -2,6 +2,6 @@ class UrlUtils{
   static const String URL_IMAGE_STORY = URL+"images/coverstory/";
   static const String URL_IMAGE_EMAGZ = URL+"emagazine/thumbnail/";
   static const String URL_FILES_EMAGZ = URL+"emagazine/files/";
-  // static const String URL = "https://pn10mobprd.ptpn10.co.id:8598/";
-  static const String URL = "https://digi.bgskr-project.my.id/";
+  static const String URL = "https://pn10mobprd.ptpn10.co.id:8598/";
+  // static const String URL = "https://digi.bgskr-project.my.id/";
 }
